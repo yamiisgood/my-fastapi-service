916ac61eaa80d5b6404f33e0ef680e75da1cf7de
goddamn.yg <williamborrega12@gmail.com>
2026-09-15 10:38:52 +0800

    Remove trailing slash from API_URL

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://my-fastapi-service-gi31.vercel.app/api/v1/";
+const API_URL = "https://my-fastapi-service-gi31.vercel.app/api/v1";
 const API_KEY = "student-api-key-123";
 
 let currentPage = 0;
